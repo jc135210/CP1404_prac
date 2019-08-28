0112571bcd4f7a6b1e253437ee4aa42373e3bb3b
adam <adam.leite@my.jcu.edu.au>
2019-08-28 14:13:33 +1000

    add functions

--- prac3/password_checker.py
+++ prac3/password_checker.py
@@ -1,7 +1,25 @@
-MIN_LENGTH = 5
-
-password = input("enter password min length of {}".format(MIN_LENGTH))
-while len(password) > MIN_LENGTH:
-    print("try again")
+def oldpassword():
+    MIN_LENGTH = 5
     password = input("enter password min length of {}".format(MIN_LENGTH))
-print('*' * len(password))
+    while len(password) > MIN_LENGTH:
+        print("try again")
+        password = input("enter password min length of {}".format(MIN_LENGTH))
+    print('*' * len(password))
+
+
+def main():
+    MIN_LENGTH = 6
+    password = get_password(MIN_LENGTH)
+    display_astrix(password)
+
+
+def get_password(length):
+    password = input("enter password min length of {}".format(length))
+    while len(password) > length:
+        print("try again")
+        password = input("enter password min length of {}".format(length))
+    return password
+
+
+def display_astrix(password):
+    print('*' * len(password))
