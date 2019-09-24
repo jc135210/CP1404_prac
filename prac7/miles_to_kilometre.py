2af63db38085a31832fd8233b68dea43d2b5c6ad
jc135210 <adam.leite@my.jcu.edu.au>
2019-09-24 20:40:57 +1000

    add 2 stage to miles to kilometre .kv and .py files

--- prac7/miles_to_kilometre.py
+++ prac7/miles_to_kilometre.py
@@ -1,26 +1,41 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 MILES_TO_KM = 1.60934
 
 
 class MilesConverterApp(App):
+    output_message = StringProperty()
+
     def build(self):
         Window.size = (400, 200)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('miles_to_kilometre.kv')
         return self.root
 
-    def handle_calculate(self):
-        value = float(self.root.ids.input_miles.text)
-        result = value * MILES_TO_KM
-        self.root.ids.output_label.text = str(result)
+    def handle_calculate(self, text):
+        value = self.validate_value(text)
+        # result = value * MILES_TO_KM
+        # self.root.ids.output_label.text = str(result)
+        self.update(value)
 
-    def handle_increment(self, change):
-        value = float(self.root.ids.input_miles.text) + change
+    def handle_increment(self, text, change):
+        value = self.validate_value(text) + change
         self.root.ids.input_miles.text = str(value)
-        self.handle_calculate()
+        # self.handle_calculate()
+
+    def update(self, value):
+        self.output_message = str(value * MILES_TO_KM)
+
+    @staticmethod
+    def validate_value(text):
+        try:
+            value = float(text)
+            return value
+        except ValueError:
+            return 0.0
 
 
 MilesConverterApp().run()
